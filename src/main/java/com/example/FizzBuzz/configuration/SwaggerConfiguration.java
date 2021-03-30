6b888146e4b175b98a64ae6e1ca6f9fde860e24a
fhery021 <fhery021@gmail.com>
2021-03-31 01:58:35 +0200

    package correction

--- src/main/java/com/example/FizzBuzz/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/FizzBuzz/configuration/SwaggerConfiguration.java
@@ -15,7 +15,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.fizzbuzz"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.FizzBuzz"))
                 .paths(PathSelectors.any())
                 .build();
     }
